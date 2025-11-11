5cfcbc2e7c62b2485b74ffaf2dcb8155940d8916
AbhiScript <abhijeetshastri08@gmail.com>
2025-11-11 23:18:15 +0530

    Home servlet added

--- src/main/java/com/blog/servlets/HomeServlet.java
+++ src/main/java/com/blog/servlets/HomeServlet.java
@@ -1,17 +1,36 @@
 package com.blog.servlets;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import com.blog.dao.PostDAO;
+import com.blog.model.Post;
 
 @WebServlet("/home")
-public class HomeServlet {
-	private PostDAO postDao = new PostDAO();
-	
+public class HomeServlet extends HttpServlet {
+    private PostDAO postDao = new PostDAO();
+
     @Override
-	protected void doGet(HttpServletRequest request,HttpServletResponse response) {
-		
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        List<Post> posts = null;
+		try {
+			posts = postDao.getAllPosts();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        request.setAttribute("posts", posts); 
+        RequestDispatcher dispatcher = request.getRequestDispatcher("home.jsp");
+        dispatcher.forward(request, response);
+    }
 }
